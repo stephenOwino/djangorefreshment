76aa70549ff975b32562b25d7f8330da6b2bceef
stephenOwino <36981339s@gmail.com>
2025-09-15 17:05:34 +0300

    added delete request for deleting drinks

--- drinks/views.py
+++ drinks/views.py
@@ -48,5 +48,8 @@ def drink_detail(request, id):
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
   elif request.method == 'DELETE':
-    pass
+    drink.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
+
+
 
